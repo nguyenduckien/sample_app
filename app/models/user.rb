c64374f46f4b40ccf71f53554dd02950e213e67e
NTK <nguyenduckien.dev@gmail.com>
2018-07-14 23:56:52 +0700

    Modeling users

---
+++ app/models/user.rb
@@ -0,0 +1,21 @@
+class User < ApplicationRecord
+  before_save :downcase_email
+
+  validates :name, presence: true, length:
+    {maximum: Settings.maximum.name_max}
+
+  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+  validates :email, presence: true, length:
+    {maximum: Settings.maximum.email_max}, format:
+    {with: VALID_EMAIL_REGEX}, uniqueness: {case_sensitive: false}
+  has_secure_password
+
+  validates :password, presence: true, length:
+    {minimum: Settings.minimum.pass_min}
+
+  private
+
+  def downcase_email
+    email.downcase!
+  end
+end
